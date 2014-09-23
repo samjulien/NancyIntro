5e949fd79bbd32b88ff5d2597c666815cc207f19
samjulien <spjulien@gmail.com>
2014-09-22 20:52:24 -0400

    Just finished "Using Curl Redux"

--- HomeModule.cs
+++ HomeModule.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Nancy;
 
 namespace NancyIntro
 {
-    public class HomeModule
+    public class HomeModule : NancyModule
     {
+        public HomeModule()
+        {
+            Func<Request, bool> _isNotApiClient = request => !request.Headers.UserAgent.ToLower().StartsWith("curl");
+
+            Get["/", ctx=> _isNotApiClient.Invoke(ctx.Request)] = p => View["index.html"];
+
+            Get["/"] = p => "Welcome to the Pluralsight API!";
+
+        }
     }
 }
