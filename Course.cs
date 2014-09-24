45a13cc9a5f8554381aecbcd682bca86bfb46651
Sam Julien <sjulien@tradepmr.com>
2014-09-24 11:50:24 -0400

    Through XML Demo
    Nothing is working though. Blah.

---
+++ Course.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace NancyIntro
+{
+	public class Repository
+	{
+		public static IList<Course> Courses = new List<Course>();
+
+		public static Course AddCourse(string name, string author)
+		{
+			return AddCourse(name, author, new string[0]);
+		}
+		public static Course AddCourse(string name, string author, string[] topics)
+		{
+			var course = new Course(Courses.NextId(), name, author);
+			topics.ToList().ForEach(course.AddModule);
+			Courses.Add(course);
+			return course;
+		}
+		public static void AddCourse(Course course)
+		{
+			course.Id = Courses.NextId();
+			course.Modules.ToList().ForEach(m => m.Id = course.Modules.NextId());
+			Courses.Add(course);
+		}
+
+		public static Course GetCourse(int id)
+		{
+			return Courses.SingleOrDefault(x => x.Id == id);
+		}
+	}
+
+	[XmlRoot("course")]
+	public class Course : IEntity
+	{
+		// Parameterless constructor needed for model binding
+		public Course()
+		{
+			Modules = new List<Module>();
+		}
+
+		public Course(int id, string name, string author)
+		{
+			Id = id;
+			Name = name;
+			Author = author;
+			Modules = new List<Module>();
+		}
+
+		[XmlAttribute("id")]
+		public int Id { get; set; }
+		[XmlElement("name")]
+		public string Name { get; set; }
+		[XmlAttribute("author")]
+		public string Author { get; set; }
+		[XmlArray("modules"), XmlArrayItem(typeof(Module), ElementName = "module")]
+		public List<Module> Modules { get; set; }
+
+		public void AddModule(string topic)
+		{
+			Modules.Add(new Module(Modules.NextId(), topic));
+		}
+	}
+
+	public class Module : IEntity
+	{
+		// Parameterless constructor needed for model binding
+		public Module()
+		{
+		}
+
+		public Module(int id, string topic)
+		{
+			Id = id;
+			Topic = topic;
+		}
+
+		[XmlAttribute("id")]
+		public int Id { get; set; }
+		[XmlElement("topic")]
+		public string Topic { get; set; }
+	}
+
+	public interface IEntity
+	{
+		int Id { get; set; }
+	}
+
+	public static class Extensions
+	{
+		public static int NextId<T>(this IList<T> list) where T : IEntity
+		{
+			return list.Any() ? list.Max(x => x.Id) + 1 : 0;
+		}
+	}
+}
--- HomeModule.cs
+++ HomeModule.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
 using System.Linq;
 using System.Web;
 using Nancy;
+using Nancy.ModelBinding;
+using Nancy.Responses;
 
 namespace NancyIntro
 {
@@ -12,10 +15,48 @@ namespace NancyIntro
         {
             Func<Request, bool> _isNotApiClient = request => !request.Headers.UserAgent.ToLower().StartsWith("curl");
 
-            Get["/", ctx=> _isNotApiClient.Invoke(ctx.Request)] = p => View["index.html"];
+            Get["/", ctx=> _isNotApiClient.Invoke(ctx.Request)] = p => View["/assets/index.html"];
 
             Get["/"] = p => "Welcome to the Pluralsight API!";
 
-        }
+			/**** Course Routes ****
+			 * 
+			 * GET list of courses
+			 * GET single course
+			 * POST single course
+			 * Return responses as JSON
+			 * 
+			 * ********************/
+
+			Get["/courses"] = p => new JsonResponse(Repository.Courses, new DefaultJsonSerializer());
+
+			Get["/courses/{id}"] = p => Response.AsJson((Course)Repository.GetCourse(p.id));
+
+			Post["/courses", c => c.Request.Headers.ContentType != "application/x-www-urlencoded"] = p =>
+			{
+				var course = this.Bind<Course>();
+				Repository.AddCourse(course);
+				return NewCourseResponse(course);
+			};
+			Post["/courses"] = p =>
+			{
+				var name = this.Request.Form.Name;
+				var author = this.Request.Form.Author;
+				var course = Repository.AddCourse(name, author);
+
+				return NewCourseResponse(course);
+			};
+		}
+
+		Response NewCourseResponse(dynamic course)
+		{
+			string url = string.Format("{0}/{1}", this.Context.Request.Url, course.Id);
+
+			return new Response()
+			{
+				StatusCode = HttpStatusCode.Accepted
+			}
+				.WithHeader("Location", url);
+		}
     }
 }
